ec6e1e1ecec07f20e8bc52510271746c4bb7b3ca
Austin.France@gmail.com <Austin.France@gmail.com@5be63bcf-75c3-2423-7759-7a9c416dda0a>
2013-09-14 12:19:36 +0000

    Switch youtube player to be activated by a / command. Demonstrates how modules can be loaded as-needed.

--- client/ui/youtube/youtube.js
+++ client/ui/youtube/youtube.js
@@ -1,6 +1,18 @@
-UI.define([ 'text!./embed.html', 'text!./embed.css' ], function(html, css) {
+UI.define([ 'chatbox', 'text!./embed.html', 'text!./embed.css' ], function(chatbox, html, css) {
 	UI.css(css);
-	var frame = UI.html(html); 
+	var frame = UI.html(html), iframe = frame.children();
 	frame.draggable();
-	return {};
+	return { 
+		play: function(video) {
+			chatbox.println('Playing video ' + video, { color: "yellow" });
+			iframe[0].src = "//www.youtube.com/embed/"+video+"?controls=0&rel=0&showinfo=0";
+			this.show();
+		},
+		show: function() {
+			frame.css({display:"block"});
+		},
+		hide: function() {
+			frame.css({display:"none"});
+		}
+	};
 });
