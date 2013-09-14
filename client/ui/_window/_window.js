d87fea21b16c118ddb9630c4c58721e8ffb79f00
Austin.France@gmail.com <Austin.France@gmail.com@5be63bcf-75c3-2423-7759-7a9c416dda0a>
2013-09-14 16:16:47 +0000

    adjust right padding of a standard window

--- client/ui/_window/_window.js
+++ client/ui/_window/_window.js
@@ -1,9 +1,9 @@
 UI.define(['text!./_window.css','text!./_window.html'],function(css,html){
-	UI.css(css);
+	UI.css(css);			// standard window CSS
 	var exports = {
 		create: function(id) {
 			var w = UI.html(html,{id:id});
-			w.content = w.children('div');
+			w.content = w.children('div');	// child element is where content goes
 			return w;
 		}
 	};
