c101858ec217a7b73b88252e57a4183db0c7ac9a
Austin.France@gmail.com <Austin.France@gmail.com@5be63bcf-75c3-2423-7759-7a9c416dda0a>
2013-09-14 16:11:29 +0000

    remove unnecessary user-guide class

--- trunk/client/ui/guide/guide.js
+++ trunk/client/ui/guide/guide.js
@@ -1,9 +1,7 @@
 UI.define(['_keyboard','_window','text!./guide.css','text!./guide.html'],function(keyboard,window,css,html){
 	UI.css(css);
-	debugger;
 	var guide = window.create('guide');
 	guide.draggable();
-	guide.content.addClass('user-guide');
 	guide.content.html(html);
 	keyboard.onkey(105,function(event) {
 		if (guide[0].style.display == 'block') {
