b7fa7be3d3daebd219e129c1cbd6c166e09d7589
Austin.France@gmail.com <Austin.France@gmail.com@5be63bcf-75c3-2423-7759-7a9c416dda0a>
2013-09-14 22:59:15 +0000

    separate out black chrome and border into separate window options

--- trunk/client/ui/guide/guide.js
+++ trunk/client/ui/guide/guide.js
@@ -1,6 +1,11 @@
 UI.define(['_keyboard','_window','text!./guide.css','text!./guide.html'],function(keyboard,window,css,html){
 	UI.css(css);
-	var guide = window.create({ id: 'guide', html: html, draggable: { cancel: '#guide>div' } });
+	var guide = window.create({
+			id: 'guide', html: html, 
+			hasChrome: true, 
+			hasBorder: true,
+			draggable: { cancel: '#guide>div' }
+	});
 	keyboard.onkey(105,function(event) {
 		if (guide[0].style.display == 'block') {
 			exports.hide();
