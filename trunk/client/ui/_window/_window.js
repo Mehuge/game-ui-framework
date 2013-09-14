a02045fd7c5ce818479a35859edc9daefca2d3f1
Austin.France@gmail.com <Austin.France@gmail.com@5be63bcf-75c3-2423-7759-7a9c416dda0a>
2013-09-14 16:58:07 +0000

    allow window creation to specify content html

--- trunk/client/ui/_window/_window.js
+++ trunk/client/ui/_window/_window.js
@@ -1,9 +1,10 @@
 UI.define(['text!./_window.css','text!./_window.html'],function(css,html){
 	UI.css(css);			// standard window CSS
 	var exports = {
-		create: function(id) {
+		create: function(id,innerHTML) {
 			var w = UI.html(html,{id:id});
 			w.content = w.children('div');	// child element is where content goes
+			if (innerHTML) w.content.html(innerHTML);
 			return w;
 		}
 	};
