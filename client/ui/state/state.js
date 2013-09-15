a351126a63f9060d4039923cea24a1f8eb2621ca
Austin.France@gmail.com <Austin.France@gmail.com@5be63bcf-75c3-2423-7759-7a9c416dda0a>
2013-09-15 12:52:55 +0000

    Add persistent storage to state object

--- client/ui/state/state.js
+++ client/ui/state/state.js
@@ -1,16 +1,51 @@
 "use strict";
 UI.define([ ], function() {
-	var _nick = "Player_"+((Math.random()*100)|0);
+
+	var storage = localStorage,
+		domain = "com.google.code.game-ui-framework",
+		key = domain + '.state',
+		state;
+
+	try {
+		state = JSON.parse(storage.getItem(key));
+	} catch(e) {
+		// no-op
+	}
+
+	function saveState() {
+		storage.setItem(domain+'.state', JSON.stringify(state));
+	}
+
+	if (!state) {
+		state = { 
+			nick: "Player_"+((Math.random()*100)|0)
+		};
+		saveState();
+	}
+
 	var exports = {
-		"chat-box": [5,5],						// position relative to bottom left
-		"player-frame": [5, 5],					// position relative to bottom right
+
 		nick: function(n) {
 			if (n) {
-				_nick = n;
-				UI.pub("PLAYER_NICK_CHANGED", _nick);
+				state.nick = n;
+				saveState();
+				UI.pub("PLAYER_NICK_CHANGED", n);
 			}
-			return _nick;
+			return state.nick;
+		},
+
+		get: function(n,d) {
+			var v = state[n];
+			return v ? v : d;
+		},
+
+		set: function(n,v) {
+			state[n] = v;
+			UI.pub("STATE_CHANGED", { name: n, value: v });
+			saveState();
 		}
+
 	};
+
 	return exports;
 });
