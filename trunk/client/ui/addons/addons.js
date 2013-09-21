4a0ef5e628657054b9b3f4a2860d9f4a84007a89
Austin.France@gmail.com <Austin.France@gmail.com@5be63bcf-75c3-2423-7759-7a9c416dda0a>
2013-09-21 12:33:17 +0000

    Add the ability to stop/start (unload/load) stoppable components (ones that defined a public stop method

--- trunk/client/ui/addons/addons.js
+++ trunk/client/ui/addons/addons.js
@@ -7,17 +7,38 @@ UI.define(['_keyboard','_window','text!./style.css','text!./content.html'],funct
 			draggable: { cancel: '#addons>div' }
 	});
 	function load() {
-		var loaded = UI.addons(), html = '', states = [ "LOADING", "INITIALISED", "DEFINED", "STARTED" ];
-		html += '<table><tr><th>Name</th><th>State</th></tr>';
+		var loaded = UI.addons(), html = '', 
+			states = [ "LOADING", "INITIALISED", "DEFINED", "STARTED" ];
+		html += '<table><tr><th>Name</th><th>State</th><th>Enabled</th></tr>';
 		for (var i = 0; i < loaded.length; i++) {
-			var addon = loaded[i];
+			var addon = loaded[i], interface = addon.interface,
+				unloaded = addon.readyState == undefined,
+				state = unloaded ? "DISABLED" : states[addon.readyState],
+				checked = !unloaded,
+				stoppable = (interface && interface.stop) || unloaded;
 			html += '<tr>'
 					+ '<td>'+addon.name+'</td>'
-					+ '<td>'+states[addon.readyState]+'</td>'
+					+ '<td>'+state+'</td>'
+					+ '<td><input type="checkbox"'
+						+ (checked ? ' checked' : '')
+						+ (stoppable ? ' i="'+i+'"' : ' disabled') + '>'
+						+ '</td>'
 					+ '</tr>';
 		}
 		html += '</table>';
 		addons.content.html(html);
+		addons.content.find('input').on('change', function(event) {
+			var i = event.target.getAttribute("i");
+			if (i) {
+				var addon = loaded[i];
+				if (addon.readyState == undefined) {
+					UI.require([ addon.name ]);
+				} else {
+					UI.unload(addon.name);
+				}
+				setTimeout(function() { load(); }, 100);
+			}
+		});
 	};
 	var exports = {
 		run: function() { },					// start is a no-op
